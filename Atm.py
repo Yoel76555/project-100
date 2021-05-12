d7fe206050fdd96c1e81a513d2c25f7ba93f41be
Yoel76555 <71801083+Yoel76555@users.noreply.github.com>
2021-05-11 20:12:44 -0600

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,14 @@
+class Atm(object): 
+    def __init__(self, users_atm, card_number, pin_number):
+        self.users_atm= users_atm
+        self.card_number= card_number
+        self.pin_number= pin_number
+        
+    
+    def CashWithdrawl(self):
+        print("CashWithdrawl")
+
+    def BalanceEnquiry(self):
+        print("BalancingEnquiry")
+    
+
